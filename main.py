11f258c1c11dc96b79d6c432ddb4011f4cbdb0c5
sergei-ladygin <s.ladygin@gmail.com>
2024-12-07 22:11:31 +0300

    added some code and module constatns

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import pygame
+from constants import *
 
 def main():
-    print("Starting asteroids!") #
-    
+    print("Starting asteroids!")
+    print(f'Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}')    
 if __name__ == "__main__":
     main()
