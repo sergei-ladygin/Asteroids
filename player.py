03c8a6bc09c6ec41b5bad79bfdce9ebd44d56470
sergei-ladygin <s.ladygin@gmail.com>
2024-12-25 21:54:39 +0300

    Modified shooting: can shoot rare now

--- player.py
+++ player.py
@@ -4,13 +4,15 @@ from circleshape import CircleShape
 from shot import Shot
 
 class Player(CircleShape):
+    
+    cooldown_timer = 0
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS) 
         self.rotation = 0
+        self.cooldown_timer = 0
         
     def triangle(self):
-        # Create a triangle pointing forward, with the tip at the player's position
-        
+        # Create a triangle pointing forward, with the tip at the player's position    
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
         a = self.position + forward * self.radius
@@ -21,11 +23,8 @@ class Player(CircleShape):
     def draw(self, screen):
         pygame.draw.polygon(screen, (255, 255, 255), self.triangle(), 4)
         
-    def rotate(self, dt):
-        self.rotation += PLAYER_TURN_SPEED * dt
-        
     def update(self, dt):
-        keys = pygame.key.get_pressed()
+        keys = pygame.key.get_pressed()       
 
         if keys[pygame.K_a]:
             self.rotate(-dt)
@@ -35,16 +34,24 @@ class Player(CircleShape):
             self.move(dt)
         if keys[pygame.K_s]:
             self.move(-dt) 
-        if keys[pygame.K_SPACE]:
+        if keys[pygame.K_SPACE] and self.cooldown_timer <= 0:
             self.shoot()
+            
+        # Decrement the cooldown timer
+        if self.cooldown_timer > 0:
+            self.cooldown_timer -= dt
+            
             
             
     def move(self, dt):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         self.position += forward * PLAYER_SPEED * dt
         
+    def rotate(self, dt):
+        self.rotation += PLAYER_TURN_SPEED * dt
+        
     def shoot(self):
         shot = Shot(self.position.x, self.position.y)
         shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
-        
+        self.cooldown_timer = PLAYER_SHOOT_COOLDOWN
         
